820ecb32025ad43599f4c57d0f43f61dbc10d598
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-17 12:25:33 +0600

    photo route

---
+++ server/routes/photo.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Photo = require('../models/photo')
+
+router.get('/getallphotos', async (req, res) => {
+    try {
+        const photos = await Photo.find({})
+        res.send(photos)
+    } catch (error) {
+        return res.status(400).json({ message: error })
+    }
+})
+
+router.post('/getphotobyid', async (req, res) => {
+    const { photoid } = req.body
+    try {
+        const photo = await Photo.findById(roomid)
+        res.send(photo)
+    } catch (error) {
+        return res.status(400).json({ message: error })
+    }
+})
+
+module.exports = router
